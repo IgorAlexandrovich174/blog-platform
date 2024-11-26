c7fda8f5355e22e6f1bb4c528b75e7426231d68c
IgorAlexandrovich <igoralexandrovich@mail.ru>
2024-11-26 19:18:45 +0500

    refact

--- src/components/Likes/Likes.jsx
+++ src/components/Likes/Likes.jsx
@@ -4,19 +4,22 @@ import {Box, IconButton} from "@mui/material";
 import {Favorite, FavoriteBorder} from "@mui/icons-material";
 
 const Likes = ({ count, slug }) => {
-    const [likeArticle, {isSuccess}] = useLikeArticleMutation();
+    const [likeArticle, {data: updatedArticle}] = useLikeArticleMutation();
+    const [like, setlike] = useState(false);
+
 
     const handleLikeClick = async () => {
         await likeArticle(slug);
+        setlike(!like);
     };
 
     return (
         <Box display="flex" alignItems="center">
             <IconButton
-                color={isSuccess ? "error" : "default"}
+                color={like ? "error" : "default"}
                 onClick={handleLikeClick}
             >
-                {isSuccess ? <Favorite /> : <FavoriteBorder />}
+                {like ? <Favorite /> : <FavoriteBorder />}
             </IconButton>
             <Box ml={1}>{count}</Box>
         </Box>
